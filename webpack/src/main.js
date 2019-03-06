cd8be030be26165d0ab7bdc9400ed87efb47d16b
Diego Maidana <deeze.designs@gmail.com>
2019-03-06 12:54:37 -0300

    Mapa de Instituciones
    + Se agrega mapa de instituciones interactivo

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 
-//Vuetify
+// Google Maps
+import * as VueGoogleMaps from "vue2-google-maps";
+
+// Vuetify
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+
+import _ from 'lodash';
 
 // Material Designs Icons
 import "vue-material-design-icons/styles.css"
@@ -16,6 +21,13 @@ import store from './store'
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBFPUFES_l4Gn4QKty9nwXTdeM-Ew-Hxb8",
+    libraries: "places" // necessary for places input
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
