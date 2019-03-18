ebc1c80aad5375389af492a321cd76c4237b575f
Diego Maidana <deeze.designs@gmail.com>
2019-03-18 10:42:01 -0300

    Modificaciones en Scroll de Mapa de Instituciones

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -19,6 +19,23 @@ import "vue-material-design-icons/styles.css"
 import router from './router'
 import store from './store'
 
+// ScrollTo
+import VueScrollTo from 'vue-scrollto';
+
+Vue.use(VueScrollTo, {
+     container: "body",
+     duration: 500,
+     easing: "ease",
+     offset: 0,
+     force: true,
+     cancelable: true,
+     onStart: false,
+     onDone: false,
+     onCancel: false,
+     x: false,
+     y: true
+ })
+
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
